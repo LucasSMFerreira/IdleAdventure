9935745c8d85fc85cae0388c73d7c7054b75e86d
LucasMartins <lucas.martins.bz@gmail.com>
2026-09-15 13:17:05 -0300

    Faz player parar perto do inimigo

--- scenes/player.gd
+++ scenes/player.gd
@@ -1,7 +1,12 @@
 extends CharacterBody2D
 
 var velocidade = 100.0
+var inimigo_perto = false
 
-func _physics_process(delta):
-	velocity.x = velocidade
+func _physics_process(_delta):
+	velocity.x = 0.0 if inimigo_perto else velocidade
 	move_and_slide()
+
+func _on_detection_body_entered(body):
+	if body.is_in_group("enemy"):
+		inimigo_perto = true
